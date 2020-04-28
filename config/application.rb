40dd7205d0f22bf13aa126a707f6473fb9fbc5c0
AMargaillan <adeline.margaillan@gmail.com>
2020-04-28 03:12:37 +0000

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Wedding2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -18,5 +19,5 @@ module Wedding2
   end
 end
 
-config.assets.initialize_on_precompile = false
+
 
