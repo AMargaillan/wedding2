0868cf2f4e30895342936dbc661c34ae76066b0a
AMargaillan <adeline.margaillan@gmail.com>
2020-05-28 01:52:33 +0000

    registry page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'invites#index'
   get '/details' => 'invites#details'
+  get '/registry' => 'invites#registry'
   resources :photos, only: :show
   
 end
